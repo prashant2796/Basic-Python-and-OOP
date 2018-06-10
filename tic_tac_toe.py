172fc431899b2d5022eeae6cad0897f529f73cd8
prashant <prashantpandey071@gmail.com>
2018-06-10 17:56:21 +0530

    Final implementation.Code Working Correctly

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,7 @@
 import random
-board = ['#','X','O','X','O','X','X','X','O','X']
+print("Welcome to TIC TAC TOE!")
+
+
 def display_board(board):
 	"""
 	This function is used to display 
@@ -17,7 +19,6 @@ def display_board(board):
 	print(' ' + board[1] +' | ' + board[2] + ' | ' + board[3])
 	print('   |   |')
 
-# display_board(['#','X','O','X','O','X','O','X','O','X'])
 
 def player_input():
 	"""
@@ -29,12 +30,13 @@ def player_input():
 	# Continue asking user until you get the valid input
 	while not (marker == 'X' or marker == 'O'):
 		marker = input("Player 1:Choose X or O: ").upper()
+		
 	# Set the marker according to the choice of first user
 	if marker == 'X':
 		return('X','O')
 	else:
 		return('O','X')
-# player_input()
+
 
 def place_marker(board,marker,position):
 	"""
@@ -42,9 +44,9 @@ def place_marker(board,marker,position):
 	on a specified postion by the user.
 	"""
 	board[position] = marker
-# place_marker(test_board,'&',6)
 
-def win_check(board,marker):
+
+def win_check(board,mark):
 	"""
 	The function takes in the board and 
 	the marker and checks to see if that
@@ -54,18 +56,15 @@ def win_check(board,marker):
 	Check if any row or column or any diagonal
 	has the same marker.
 	"""
-	for i in range(1,10):
-		# Check to see if there is consecutive marker in a row.If Yes,return True
-		if board[i] == marker and board[i+1] == marker and board[i+2] == marker:
-			return True
-		# Check to see if there is consecutive marker in a column.If Yes,return True
-		elif board[i] == marker and board[i+3] == marker and board[i+6] == marker:
-			return True
-		# Check to see if there is consecutive marker in a diagonal.If Yes,return True
-		elif board[i] == marker and board[i+4] == marker and board[i+8] == marker:
-			return True
-		else:
-			return False
+	return ((board[7] ==  board[8] ==  board[9] == mark) or # across the top
+    (board[4] ==  board[5] ==  board[6] == mark) or # across the middle
+    (board[1] ==  board[2] ==  board[3] == mark) or # across the bottom
+    (board[7] ==  board[4] ==  board[1] == mark) or # down the left side
+    (board[8] ==  board[5] ==  board[2] == mark) or # down the middle
+    (board[9] ==  board[6] ==  board[3] == mark) or # down the right side
+    (board[7] ==  board[5] ==  board[3] == mark) or # diagonal
+    (board[9] ==  board[5] ==  board[1] == mark)) # diagonal
+
 
 def choose_first():
 	"""
@@ -81,12 +80,14 @@ def choose_first():
 	else:
 		return 'Player 2'
 
+
 def space_check(board,position):
 	"""
 	This function returns boolean indicating whether the 
 	space on the board is freely available.
 	"""
 	return board[position] == ' '
+
 
 def full_board_check(board):
 	"""
@@ -96,8 +97,9 @@ def full_board_check(board):
 	for i in range(1,10):
 		if space_check(board,i):
 			return False
-	# Board is full if we return True
+	#  We return True Board is full if
 	return True
+
 
 def player_choice(board):
 	"""
@@ -108,7 +110,7 @@ def player_choice(board):
 	position = 0
 
 	while position not in range(1,10) or not space_check(board,position):
-		position = int(input("choose a position: (1-9)"))
+		position = int(input("choose a position: (1-9) "))
 
 	return position
 
@@ -117,12 +119,89 @@ def replay():
 	This function asks the player if they want to play again and return 
 	a boolean True if they want to play again
 	"""
-	choice = input("Play Again? Enter Yes or No").upper()
+	choice = input("Play Again? Enter Yes or No ").upper()
 	return choice == 'YES'
-	
-win_check(board,'X')
-display_board(board)
 
+
+def tictactoe_game():
+	"""
+	This function runs all the other functions
+	for the actual game play.
+	"""
+
+	# While loop to keep running the game.
+	while True:
+		# Set up the board and the marker
+		game_board = [' ']*10
+		player1_marker,player2_marker = player_input()
+
+		# Choose which User will go first
+		turn = choose_first()
+		print(turn + 'will go first')
+	    
+		play_game = input("Ready to play Tic Tac Toe? y or n ").lower()
+
+		if play_game == 'y':
+			game_on = True
+		else:
+			game_on = False
+
+		while game_on:
+
+			if turn == 'Player 1':
+				# Show the Board
+				display_board(game_board)
+
+				# Choose a position
+				position = player_choice(game_board)
+
+				# Place the marker on the position
+				place_marker(game_board,player1_marker,position)
+
+				# Check if they won
+				if win_check(game_board,player1_marker):
+					display_board(game_board)
+					print("Player 1 has Won!!")
+					game_on = False
+				else:
+					# Check whether there is a Tie
+					if full_board_check(game_board):
+						display_board(game_board)
+						print("The Game is a tie!!")
+						game_on = False
+					else:
+						# If not return the turn to player 2
+						turn = 'Player 2'
+
+			else:
+				# Show the Board
+				display_board(game_board)
+
+				# Choose a position
+				position = player_choice(game_board)
+
+				# Place the marker on the position
+				place_marker(game_board,player2_marker,position)
+
+				# Check if they won
+				if win_check(game_board,player2_marker):
+					display_board(game_board)
+					print("Player 2 has Won!!")
+					game_on = False
+				else:
+					# Check whether there is a Tie
+					if full_board_check(game_board):
+						display_board(game_board)
+						print("The Game is a tie!!")
+						game_on = False
+					else:
+						# If not return the turn to player 1
+						turn = 'Player 1'
+	    		
+		if not replay():
+			break
+
+tictactoe_game()
 
 
 
